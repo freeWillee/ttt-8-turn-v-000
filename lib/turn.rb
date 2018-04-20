0370d1400afeb99df0d269f2bd5ba62268718bd7
Will Lee <will.i.lee@outlook.com>
2018-04-20 17:52:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ end
 # 1) if user's input is > 8, then false
 # 2) if space is occupied, then false --> defind a separate #position_taken? method.
 def valid_move?(board, index)
-  if board[index] > 8 || position_taken?(board, index)
+  if board[index].to_i > 8 || position_taken?(board, index)
     true
   else
     false
