537ebfb29840e41fb4163da7eb2802487549f5a1
Will Lee <will.i.lee@outlook.com>
2018-04-20 17:37:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 
 # displays the board
-def display_board(board)  
-  puts " #{board[0]} | #{board[1]} | #{board[2]} " 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
