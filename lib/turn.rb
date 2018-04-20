6e71665b6c87ac68cad6881216f89c41eaeb7531
Will Lee <will.i.lee@outlook.com>
2018-04-20 17:36:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,15 @@
 
-def display_board(board)  # displays the board
+# displays the board
+def display_board(board)  
   puts " #{board[0]} | #{board[1]} | #{board[2]} " 
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#converts user input to an integer (from a string)
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
 end
